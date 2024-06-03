344af44a95142343786e6dfab15135c71b9f99f8
kunal <deopakunal@gmail.com>
2024-06-03 12:22:11 +0530

    Config issue resolved

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import app from "./src/app";
 import { config } from "./src/config/config";
 const startServer = () => {
-  const port = config.port || 3000 ;
+  const port =  config.port || 3001 ;
   app.listen(port, () => {
     console.log(`welcome from the server ${port}`);
   });
