05279bd2b0d03c4b69e33d86b2503081d9063211
kunal <deopakunal@gmail.com>
2024-06-07 20:57:59 +0530

    addes global errorhandler

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,6 +1,19 @@
 import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  res.json({message: "User Created" });
+     console.log("reqdata",req.body);
+     return res.json({});
+  const { name, email, password } = req.body;
+  // validation
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+   
+  }
+//    return res.json({ message: "All fields must be filled." });
+  //process
+  //resonse
+  res.json({ message: "User Created" });
 };
-export {createUser};
+export { createUser };
