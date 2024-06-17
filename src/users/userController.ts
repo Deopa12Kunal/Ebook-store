9e056eaa50132998a278d2f98a968a010eaf0920
kunal <deopakunal@gmail.com>
2024-06-17 14:17:24 +0530

    added hash in to store password

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
-
+import bcrypt from "bcrypt";
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   //  console.log("reqdata",req.body);
   //  return res.json({});
@@ -12,11 +12,14 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     return next(error);
   }
   // database call
-  const user = await userModel.findOne({email});
-  if(user){
-    const error = createHttpError(400,"user already exists with this email");
+  const user = await userModel.findOne({ email });
+  if (user) {
+    const error = createHttpError(400, "user already exists with this email");
     return next(error);
   }
+  //Todo: Hasing password using a library bcrypt
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   //    return res.json({ message: "All fields must be filled." });
   //process
   //resonse
