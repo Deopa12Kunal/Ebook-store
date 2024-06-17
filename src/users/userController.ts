e6dab84a1caf444f30cac2e885900b63f6154261
kunal <deopakunal@gmail.com>
2024-06-17 14:06:03 +0530

    add user model

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   //  console.log("reqdata",req.body);
@@ -8,6 +9,12 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   // validation
   if (!name || !email || !password) {
     const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+  // database call
+  const user = await userModel.findOne({email});
+  if(user){
+    const error = createHttpError(400,"user already exists with this email");
     return next(error);
   }
   //    return res.json({ message: "All fields must be filled." });
