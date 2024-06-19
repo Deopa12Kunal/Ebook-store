1fcf7be4a10bd1b4cedaf8ac51cabf3b91c61de1
kunal <deopakunal@gmail.com>
2024-06-19 13:06:25 +0530

    setup multer

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,10 +1,15 @@
 import {Request, Response, NextFunction } from "express-serve-static-core";
 
 const  createBook = async(
+     //data we received from req body 
+     // we will receive data in different forn that is (form-data) not json data
+
     req: Request,
     res: Response,
-    next: NextFunction
+    next: NextFunction,
+   
 ) =>{
+     const {} = req.body;
 res.json({});
 };
 export {createBook};
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,7 +1,17 @@
 import express from "express";
 import { createBook } from "./bookController";
+import path from 'node:path';
+import multer from "multer";
 const bookRouter = express.Router();
+
+const upload =multer({
+dest:path.resolve(__dirname,'../../public/data/uploads'),
+limits:{fieldSize: 3e7} // defines the size that is 30 mb
+})
 // routes
-bookRouter.post("/", createBook);
+bookRouter.post("/",upload.fields([
+{name:'coverImage',maxCount:1},
+{name: "file", maxCount:1},
+]), createBook);
 
 export default bookRouter;
