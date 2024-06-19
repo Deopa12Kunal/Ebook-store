7f16a959a6d1bc5ee67951ba07a01d3ca7eb8e66
kunal <deopakunal@gmail.com>
2024-06-19 12:29:24 +0530

    add book model

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from "express";
 // import { config } from "./config/config";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./users/userRouters";
+import bookRouter from "./book/bookrouter";
 
 const app = express();
 app.use(express.json());
@@ -15,6 +16,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to ebook api" });
 });
 app.use("/api/users", userRouter);
+app.use('/api/books',bookRouter)
 // global error handler
 // should be at last of all routes
 app.use(globalErrorHandler);
