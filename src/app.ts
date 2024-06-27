d47f699c8fdadf1bccc70d08e911828135ef6b53
kunal <deopakunal@gmail.com>
2024-06-27 15:34:42 +0530

    Add cors support

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express from "express";
 // import createHttpError from "http-errors";
-// import { config } from "./config/config";
+import { config } from "./config/config";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./users/userRouters";
-import bookRouter from "./book/bookrouter";
+import bookRouter from "./book/bookRouter";
+import cors from "cors";
+// import { config } from "dotenv";
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+  origin: config.frontendDomain,
+  
+})
+);
 // routes
 //  app.use(express.json());
 app.get("/", (req, res) => {
--- src/config/config.ts
+++ src/config/config.ts
@@ -8,6 +8,7 @@ const _config = {
   cloudinaryCloud: process.env.CLOUDINARY_CLOUD,
   cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
   cloudinarySecret: process.env.CLOUDINARY_API_SECRET,
+  frontendDomain: process.env.FRONTEND_DOMAIN,
 
   // SPECIAL ENV VARIABLES IS USED TO IDENTIFY WHICH ENV IT IS DEV OR PRODUCTION
   // jwtSecret: process.env.JWT_SECRET,
